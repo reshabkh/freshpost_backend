55b0ed7eefff5a8c5cf0ea7461975dc0cb3ffd31
Hardik Sakpal <40515075+scratch-coder-2507@users.noreply.github.com>
2025-05-03 06:30:32 +0000

    http module added

--- src/generation/generation.module.ts
+++ src/generation/generation.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { GenerationController } from './generation.controller';
 import { GenerationService } from './generation.service';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
+  imports: [HttpModule],
   controllers: [GenerationController],
-  providers: [GenerationService]
+  providers: [GenerationService],
 })
 export class GenerationModule {}
