44fad67bef24d491a5e9a7c107fe7db8b9fd2524
Hardik Sakpal <40515075+scratch-coder-2507@users.noreply.github.com>
2025-05-03 05:59:51 +0000

    Generation module added

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { GenerationModule } from './generation/generation.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, GenerationModule],
   controllers: [AppController],
   providers: [AppService],
 })
